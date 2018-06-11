27755593cdf44074e53c28023f051cffc9ad77c5
luccab <39514683+luccab@users.noreply.github.com>
2018-06-11 00:48:40 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   root 'application#hello'
-=======
+
   root 'application#goodbye'
->>>>>>> d15ab3c8057bfc75cfd76cb508e85ae57600773a
 end
